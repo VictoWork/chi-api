31febb273433199b1ceec2a19242ce095c1e265c
victowork <vichu@live.in>
2024-08-06 22:22:26 +0400

    Added Order API with order Handler

--- main.go
+++ main.go
@@ -1,29 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/victowork/chi-api/application"
 )
 
 func main() {
 
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
-	router.Get("/hello", basicHandler)
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: router,
-	}
-
-	err := server.ListenAndServe()
+	app := application.New()
+	err := app.Start(context.TODO())
 	if err != nil {
-		fmt.Println("failed to start the sever", err)
+		fmt.Println("failed to start server", err)
 	}
-}
 
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, World"))
 }
