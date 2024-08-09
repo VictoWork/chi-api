ea5863987a49c681bde42f53a23a5c3d2fcaa425
victowork <vichu@live.in>
2024-08-09 20:06:04 +0400

    completed create endpoint

--- application/routes.go
+++ application/routes.go
@@ -6,22 +6,27 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/victowork/chi-api/handler"
+	"github.com/victowork/chi-api/repository/order"
 )
 
-func loadRoutes() *chi.Mux {
+func (a *App) loadRoutes() {
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
-	router.Route("/orders", loadOrderRoutes)
-	return router
+	router.Route("/orders", a.loadOrderRoutes)
+	a.router = router
 }
 
-func loadOrderRoutes(router chi.Router) {
+func (a *App) loadOrderRoutes(router chi.Router) {
 
-	orderHandler := &handler.Order{}
+	orderHandler := &handler.Order{
+		Repo: &order.RedisRepo{
+			Client: a.rdb,
+		},
+	}
 
 	router.Post("/", orderHandler.Create)
 	router.Get("/", orderHandler.List)
--- handler/order.go
+++ handler/order.go
@@ -1,14 +1,58 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/victowork/chi-api/model"
+	"github.com/victowork/chi-api/repository/order"
 )
 
 type Order struct {
+	Repo *order.RedisRepo
 }
 
 func (o *Order) Create(w http.ResponseWriter, r *http.Request) {
+
+	var body struct {
+		CustomerID uuid.UUID        `json:"customer_id"`
+		LineItems  []model.LineItem `json:"line_items"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		fmt.Println("error decoding request body", err)
+		return
+	}
+	now := time.Now().UTC()
+	order := model.Order{
+		OrderID:    rand.Uint64(),
+		CustomerID: body.CustomerID,
+		LineItems:  body.LineItems,
+		CreatedAt:  &now,
+	}
+
+	err := o.Repo.Insert(r.Context(), order)
+	if err != nil {
+		fmt.Println("failed to insert order", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	res, err := json.Marshal(order)
+	if err != nil {
+		fmt.Println("failed to marshal", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(res)
+	w.WriteHeader(http.StatusCreated)
+
 	fmt.Println("Create an order")
 }
 
